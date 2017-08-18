fc67d26c06fa040ce6c23127c6eaed53ca51cdab
broleomo <brooklynnmoor@gmail.com>
2017-08-18 10:52:01 -0400

    updated assignment

--- index.js
+++ index.js
@@ -1,8 +1,98 @@
 const express = require('express');
 let app = express();
 const session=require('express-session');
-const validator = require('express-validator');
+const mustacheExpress = require('mustache-express');
+// const validator = require('express-validator');
 const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine('mustache',mustacheExpress());
+app.set('views','./views');
+app.set('view engine', 'mustache');
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized:true
+}));
+
+app.get('/',(req,res)=> {
+let myUser = {};
+myUser.username = req.session.username
+myUser.password = req.session.password
+if(typeof req.session.username !== 'undefined'){
+  res.render('index',myUser);
+} else {
+  res.redirect('/login');
+}
+});
+app.get('/login',(req,res)=> {
+  res.render('login');
+});
+
+const users = {
+  Brooklynn:'12345',
+  Jon: '1234'
+};
+
+
+app.post('/login/auth',(req,res)=> {
+  // console.log(req.body);
+let name = req.body.username;
+let password = req.body.password;
+  if(users[name] === password){
+    req.session.username = name
+    req.session.password = password
+    res.redirect('/');
+  }else {
+    res.redirect('/login');
+  }
+});
+
+
+
+app.listen(3000, () => console.log("Check this out"));
+
+
+
+//   req.checkBody({
+//     'password':{
+//     notEmpty: true,
+//     isLength: {
+//       options: {min: 8},
+//       errorMessage: "Must be at least 8 characters long"
+//           },
+//     errorMessage: 'Invalid Password'
+//   },
+//   'username':{
+//     notEmpty:true,
+//   errorMessage:'Invalid Username'
+// }
+// });
+//   let errors = req.validationErrors();
+//   if (errors){
+//     let errorMessage = "You did not fill in a proper username and password";
+//     return (errorMessage);
+//     res.redirect('/');
+//   } else {
+//     res.send(`
+//   <h1> Welcome to the Upside Down </h1>
+//   <img src="https://media.giphy.com/media/l0MYOGb8rqLOug1gY/giphy.gif">
+//   <p> You are logged in under '${name}'.</p>
+//       `);
+//   };
+//
+// });
+
+
+
+
+
+
+
+
+
 
 // app.use(session({
 //   secret: 'keyboard cat',
@@ -24,55 +114,15 @@ const bodyParser = require('body-parser');
   //   res.redirect('/');
   // });
 
-app.use(validator());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(validator());
 
-app.get('/',(req,res)=> {
-  let login =
-    `
-    <h1>Login Page </h1>
-    <form action="/" method="post">
-    <input type="text" name="username" value="" placeholder="username">
-    <input type=password name="password" value="" placeholder="password">
-    <button>Login</button>
-    </form>
-    `;
-      res.send(login);
-  });
-
-app.post('/',(req,res)=> {
-  console.log(req.body);
-  let name = req.body.username;
-  let password=req.body.password;
-
-  req.checkBody({
-    'password':{
-    notEmpty: true,
-    isLength: {
-      options: {min: 8},
-      errorMessage: "Must be at least 8 characters long"
-          },
-    errorMessage: 'Invalid Password'
-  },
-  'username':{
-    notEmpty:true,
-  errorMessage:'Invalid Username'
-}
-});
-  let errors = req.validationErrors();
-  if (errors){
-    let errorMessage = "You did not fill in a proper username and password";
-    return (errorMessage);
-    res.redirect('/');
-  } else {
-    res.send(`
-  <h1> Welcome to the Upside Down </h1>
-  <img src="https://media.giphy.com/media/l0MYOGb8rqLOug1gY/giphy.gif">
-  <p> You are logged in under '${name}'.</p>
-      `);
-  };
-
-});
-
-app.listen(3000, () => console.log("Check this out"));
+// let login =
+//   `
+//   <h1>Login Page </h1>
+//   <form action="/" method="post">
+//   <input type="text" name="username" value="" placeholder="username">
+//   <input type=password name="password" value="" placeholder="password">
+//   <button>Login</button>
+//   </form>
+//   `;
+//     res.send(login);
